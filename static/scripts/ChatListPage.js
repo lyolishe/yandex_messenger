0634446d6674c12b755d7c52a863aa05432d599a
ButtSurfer <fisher10058@mail.ru>
2020-10-30 18:33:26 +0300

    файлики

---
+++ static/scripts/ChatListPage.js
@@ -0,0 +1,39 @@
+var _a, _b, _c;
+import Block from "../src/modules/Block.js";
+import { api, render } from "../src/modules/Utilits.js";
+import { UserBlock } from "../src/modules/components/UserBlock/UserBlock.js";
+import { NavTabsBlock } from "../src/modules/components/NavTabs/NavTabsBlock.js";
+import { ChatListBlock } from "../src/modules/components/ChatListBlock/ChatList.js";
+import { ChatItemBlock } from "../src/modules/components/ChatItemBlock/ChatItemBlock.js";
+import { BlankDialogBlock, blankDialogPageInitProps } from "../src/modules/components/BlankDialogBlock/BlankDialogBlock.js";
+let sideBar = new Block("aside", { classList: ["appSideBar"] });
+let main = new Block("main", {});
+render('root', sideBar.element);
+render('root', main.element);
+api('../api/userBlock.json').then(user => {
+    var _a;
+    const userBlock = new UserBlock(user);
+    (_a = sideBar.element) === null || _a === void 0 ? void 0 : _a.appendChild(userBlock.element);
+}).then(() => {
+    var _a;
+    (_a = sideBar.element) === null || _a === void 0 ? void 0 : _a.appendChild(new NavTabsBlock().element);
+}).then(() => {
+    api('../api/chatList.json').then(chatList => {
+        var _a;
+        const chats = new ChatListBlock();
+        chatList.forEach(chatItem => {
+            var _a;
+            const chat = new ChatItemBlock(Object.assign({ onClick: chats.onClick }, chatItem));
+            chats.children.push(chat);
+            (_a = chats.element) === null || _a === void 0 ? void 0 : _a.appendChild(chat.element);
+        });
+        (_a = sideBar.element) === null || _a === void 0 ? void 0 : _a.appendChild(chats.element);
+    });
+});
+const app = new Block('div', { classList: ["appMain"] });
+const dialogBlock = new BlankDialogBlock(blankDialogPageInitProps);
+(_a = app.element) === null || _a === void 0 ? void 0 : _a.append(dialogBlock.element);
+const appWrapper = new Block('div', { classList: ["appWrapper"] });
+(_b = appWrapper.element) === null || _b === void 0 ? void 0 : _b.append(app.element);
+(_c = main.element) === null || _c === void 0 ? void 0 : _c.append(appWrapper.element);
+//# sourceMappingURL=ChatListPage.js.map
---
+++ static/src/modules/components/BlankDialogBlock/BlankDialogBlockTmpl.js
@@ -0,0 +1,9 @@
+export const blankDialogBlockTmpl = `
+        <div class="col mxAuto">
+            <div class="iconPreview">
+                ЛОГО В РАЗРАБОТКЕ
+            </div>
+            <button class="{{buttonClasses}}" onclick=onNewDialog>{{text}}</button>
+        </div>
+`;
+//# sourceMappingURL=BlankDialogBlockTmpl.js.map
