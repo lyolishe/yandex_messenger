857eb1ef60e439f42cde297abfd02143a8480d96
ButtSurfer <fisher10058@mail.ru>
2020-11-08 09:05:53 +0300

    убрал лишние js-ки

--- static/outDir/src/data/Contracts.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=Contracts.js.map
--- static/outDir/src/modules/components/DiaogNavBlock/DialogNavTmpl.js
+++
@@ -1,7 +0,0 @@
-export const dialogNavTmpl = `
-    <div class="navMenu">
-        <button class="btn mdi mdi-star-outline"></button>
-        <button class="btn mdi mdi-dots-vertical"></button>
-    </div>
-`;
-//# sourceMappingURL=DialogNavTmpl.js.map
