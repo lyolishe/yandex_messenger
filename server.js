9e04573154519a0abb90c8997387840c465c4f33
ButtSurfer <fisher10058@mail.ru>
2020-12-02 19:07:46 +0300

    add server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('./static'));
+app.use((req, res) => {
+    res.sendFile(`${__dirname}/static/index.html`);
+})
+
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}!`);
+});
